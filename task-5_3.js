545924fb5fbada8f4d3e2f8b43bbd652c2cba273
Ivan Chukhalo <ivanchukhalo@gmail.com>
2020-02-24 22:05:56 +0200

    task 5.3 is added

---
+++ task-5_3.js
@@ -0,0 +1,38 @@
+"use strict";
+console.log("________________task 5.3________________");
+
+class Storage {
+  constructor(arrayOfStuff) {
+    this.items = arrayOfStuff;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  removeItem(item) {
+    if (this.items.includes(item)) {
+      this.items.splice(this.items.indexOf(item), 1);
+    }
+  }
+}
+
+const storage = new Storage([
+  "Нанитоиды",
+  "Пролонгер",
+  "Железные жупи",
+  "Антигравитатор"
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem("Дроид");
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem("Пролонгер");
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
